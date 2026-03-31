d9d68de7a15007b44f062928932e2d68a76a4768
26YAI01-YuleydiAjxup <ajxupyuleydi15db@gmail.com>
2026-03-30 20:07:18 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,27 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Inciso1_Actividad1_Leccion_7_Promedio 
+namespace Inciso2_Act1_Lecc7
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            double n1, n2, n3, n4, n5, promedio;
-            Console.WriteLine("Ingrese 5 notas");
-            n1 = double.Parse(Console.ReadLine());
-            n2= double.Parse(Console.ReadLine());
-            n3= double.Parse(Console.ReadLine());
-            n4= double.Parse(Console.ReadLine());
-            n5 = double.Parse(Console.ReadLine());
+            int edad;
+            Console.WriteLine("Ingrese su edad en números");
+            edad = int.Parse(Console.ReadLine());
 
-            promedio = (n1 + n2 + n3 + n4 + n5) / 5;
-
-            if (promedio >= 70){
-                Console.WriteLine("Aprobado");
+            if (edad>= 18) {
+                Console.WriteLine("Es mayor de edad");
             }
             else{
-                Console.WriteLine("Reprobado");
+                Console.WriteLine("Es menor de edad");
             }
         }
     }
