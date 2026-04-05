d7a5eba782b7e1e7c976b006250774064e47386a
26YAI01-YuleydiAjxup <ajxupyuleydi15db@gmail.com>
2026-04-05 16:40:44 -0600

    Add files via upload

---
+++ EjemploDeLaPágina86.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploDeLaPágina86
+{
+    internal class EjemploDeLaPágina86 
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            Console.WriteLine("Introduzca un número:");
+            num = int.Parse(Console.ReadLine());
+
+            if (num < 0)
+            {
+                Console.WriteLine("El número es menor a cero");
+            }
+            else{
+                Console.WriteLine("El número es igual o mayor a cero");
+            }
+        }
+    }
+}
---
+++ EjemploDeLaPágina86.psc
@@ -0,0 +1,13 @@
+Algoritmo EjemploDeLaPagina86
+	Definir num Como Entero
+	
+	Escribir "Introduzca un número:"
+    Leer num
+    
+    Si num < 0 Entonces
+        Escribir "El número es menor a cero"
+    Sino
+        Escribir "El número es igual o mayor a cero"
+    FinSi
+	
+FinAlgoritmo
