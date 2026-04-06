89877a472b539b9bc0a526b364b131202708cc03
26YAI01-YuleydiAjxup <ajxupyuleydi15db@gmail.com>
2026-04-05 18:56:11 -0600

    Add files via upload

---
+++ Act2_Lecc20_Inciso1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act2_Lecc20_Inciso1_Contraseña 
+{
+    internal class Act2_Lecc20_Inciso1 
+    {
+        static void Main(string[] args)
+        {
+            string clave;
+
+            Console.WriteLine("Ingrese la contraseña");
+            clave = Console.ReadLine();
+
+            if (clave == "Password123") {
+                Console.WriteLine("Bienvenido");
+            }
+            else{
+                Console.WriteLine("Acceso Prohibido");
+            }
+
+        }
+    }
+}
---
+++ Act2_Lecc20_Inciso1_Contraseña.psc
@@ -0,0 +1,12 @@
+Algoritmo Act2_Lecc20_Inciso1_Contraseña
+	Definir clave Como Cadena 
+	Escribir "Ingrese la contraseña:"
+    Leer clave
+    
+    Si clave = "Password123" Entonces
+        Escribir "Bienvenido"
+    Sino
+        Escribir "Acceso prohibido"
+    FinSi
+	
+FinAlgoritmo
