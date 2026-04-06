d214417a7d20ba85b0dfe8fa2005aa68d3bff610
26YAI01-YuleydiAjxup <ajxupyuleydi15db@gmail.com>
2026-04-05 19:02:43 -0600

    Add files via upload

---
+++ Act3_Lecc20_Inciso2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act3_Lecc20_Inciso2_Aprobado_Reprobado 
+{
+    internal class Act3_Lecc20_Inciso2 
+    {
+        static void Main(string[] args)
+        {
+            double nota;
+
+            Console.WriteLine("Ingrese la calificación:");
+            nota = double.Parse(Console.ReadLine());
+
+            if (nota >= 70) {
+                Console.WriteLine("Aprobado");
+            }
+            else {
+                Console.WriteLine("Reprobado");
+            }
+        }
+    }
+}
---
+++ Act3_Lecc20_Inciso2_Aprobado_Reprobado.psc
@@ -0,0 +1,13 @@
+Algoritmo Act3_Lecc20_Inciso2_Aprobado_Reprobado
+	Definir nota Como Real
+	
+	Escribir "Ingrese la calificación:"
+    Leer nota
+    
+    Si nota >= 70 Entonces
+        Escribir "Aprobado"
+    Sino
+        Escribir "Reprobado"
+    FinSi
+	
+FinAlgoritmo
